60d81a71e466bf459404f6e4f0c22814bca3b15c
pandurangrs <pandurangs204@gmail.com>
2023-01-19 18:22:00 +0530

    create CRUD operation using spring configuration xml file

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,8 +1,12 @@
 package com.spring.jdbc;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entity.Student;
 
 /**
  * Hello world!
@@ -18,14 +22,53 @@ public class App
     	
         ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
         
-        JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate",JdbcTemplate.class);
+         StudentDao studentDao = context.getBean("studentdao",StudentDao.class);
+         
+         //Insert Data
+//         Student student=new Student();
+//         student.setId(4);
+//         student.setName("Vikas");
+//         student.setCity("Ambegao");
+//         
+//         int result = studentDao.insert(student);
+//         System.out.println("Student added "+result);
         
-        //insert query
-        String query="insert into student(id,name,city) values(?,?,?)";
-        
-        //fire query
-        int result = jdbcTemplate.update(query,5,"Prathamesh Sharma","Khadki");
-        System.out.println("number of record inserted.. "+result);
+         
+         //Update data
+         
+//         Student student=new Student();
+//         
+//         student.setId(5);
+//         student.setName("Viki");
+//         student.setCity("Hari");
+//         int r = studentDao.change(student);
+//         System.out.println("Change student "+r);
+         
+         
+         //Delete data
+//         int r = studentDao.delete(4);
+//        System.out.println("Delete Data "+r);
+         
+         //get single data for id
+//         Student r = studentDao.getStudent(5);
+//         System.out.println("get Data "+r);
+   
+         List<Student> allStudent = studentDao.getAllStudent();
+         System.out.println("All data get = "+allStudent);
+         for (Student students : allStudent) {
+			System.out.println(students);
+		}
+         
+         
+        //1St method
+//        JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate",JdbcTemplate.class);
+//        
+//        //insert query
+//        String query="insert into student(id,name,city) values(?,?,?)";
+//        
+//        //fire query
+//        int result = jdbcTemplate.update(query,5,"Prathamesh Sharma","Khadki");
+//        System.out.println("number of record inserted.. "+result);
         
         
     }
--- src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -1,9 +1,68 @@
 package com.spring.jdbc.dao;
 
-public class StudentDaoImpl implements StudentDao{
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring.jdbc.entity.Student;
+
+public class StudentDaoImpl implements StudentDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
 	
-	public int insert() {
-		return 0;
+	//insert data
+	public int insert(Student student) {
+        //insert query
+        String query="insert into student(id,name,city) values(?,?,?)";
+        int r = this.jdbcTemplate.update(query,student.getId(),student.getName(),student.getCity());
+		return r;
+	}
+	
+	
+	
+	//update data
+	public int change(Student student) {
+		
+		String query="update student set name=?,city=? where id=?";
+		int r=this.jdbcTemplate.update(query,student.getName(),student.getCity(),student.getId());
+		return r;
+	}
+	
+	//delete data
+	public int delete(int studentId) {
+		String query="delete from student where id=?";
+		int r = this.jdbcTemplate.update(query,studentId);
+		return r;
+	}
+	
+	public Student getStudent(int studentId) {
+		
+		
+		String query="select * from student where id=?";
+		RowMapper<Student> rowMapper= new RowMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query, rowMapper,studentId);
+		
+		//select single student data
+		
+		
+		return student;
+	}
+	
+	public List<Student> getAllStudent(){
+		
+		String query="select * from student";
+		List<Student> students=this.jdbcTemplate.query(query,new RowMapperImpl());
+		return students;
 	}
 
 }
